85f73ffddab2e608b209866c1be73c3075e73476
Jdilsham <janithadilsham@gmail.com>
2025-12-10 20:04:01 +0530

    update 3

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ resource "google_container_cluster" "gke_cluster" {
   name     = "my-gke-cluster"
   location = var.zone
 
+  deletion_protection = false
+
   remove_default_node_pool = true
   initial_node_count       = 1
 
@@ -34,3 +36,37 @@ resource "google_container_cluster" "gke_cluster" {
 
 
 }
+
+resource "google_container_node_pool" "primary_nodes" {
+  name = "primary-node-pool"
+  location = var.zone
+    cluster  = google_container_cluster.gke_cluster.name
+
+    node_count = 1
+
+    node_config {
+      machine_type = "e2-medium"
+      disk_size_gb = 20
+      disk_type = "pd-standard"
+      oauth_scopes = [ 
+        "https://www.googleapis.com/auth/cloud-platform"
+       ]
+
+       labels = {
+         environment = "dev"
+       }
+
+       tags = [ "gke-node" ]
+    }
+
+    autoscaling {
+      min_node_count = 1
+      max_node_count = 2
+    }
+
+    management {
+      auto_repair = true
+      auto_upgrade = true
+    }
+}
+
