3128efd4d7ec85ece38a910f58082eeb5c75ea21
Jdilsham <janithadilsham@gmail.com>
2025-12-12 17:15:50 +0530

    update 4

--- main.tf
+++ main.tf
@@ -38,35 +38,67 @@ resource "google_container_cluster" "gke_cluster" {
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  name = "primary-node-pool"
+  name     = "primary-node-pool"
   location = var.zone
-    cluster  = google_container_cluster.gke_cluster.name
+  cluster  = google_container_cluster.gke_cluster.name
 
-    node_count = 1
+  node_count = 1
 
-    node_config {
-      machine_type = "e2-medium"
-      disk_size_gb = 20
-      disk_type = "pd-standard"
-      oauth_scopes = [ 
-        "https://www.googleapis.com/auth/cloud-platform"
-       ]
+  node_config {
+    machine_type    = "e2-medium"
+    disk_size_gb    = 20
+    disk_type       = "pd-standard"
+    image_type      = "COS_CONTAINERD"
+    service_account = google_service_account.gke_nodes.email
 
-       labels = {
-         environment = "dev"
-       }
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
 
-       tags = [ "gke-node" ]
+    labels = {
+      environment = "dev"
     }
 
-    autoscaling {
-      min_node_count = 1
-      max_node_count = 2
-    }
+    tags = ["gke-node"]
+  }
 
-    management {
-      auto_repair = true
-      auto_upgrade = true
-    }
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 2
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+}
+
+resource "google_service_account" "gke_nodes" {
+  account_id   = "gke-node-sa"
+  display_name = "GKE Node Service Account"
+}
+
+resource "google_project_iam_member" "gke_nodes_logging" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.gke_nodes.email}"
+}
+
+resource "google_project_iam_member" "gke_nodes_monitoring" {
+  project = var.project_id
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.gke_nodes.email}"
+}
+
+resource "google_project_iam_member" "gke_node_storage" {
+  project = var.project_id
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${google_service_account.gke_nodes.email}"
+}
+
+resource "google_project_iam_member" "gke_node_service_agent" {
+  project = var.project_id
+  role    = "roles/container.nodeServiceAgent"
+  member  = "serviceAccount:${google_service_account.gke_nodes.email}"
 }
 
